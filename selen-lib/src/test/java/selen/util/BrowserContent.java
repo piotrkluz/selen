1893feff611ea8f9e7e59f76a4358b325bbc2b2e
Piotr Kluz <piox89@gmail.com>
2021-01-13 01:42:09 +0100

    [TEST] Add JS tools for browser wrapper

--- selen-lib/src/test/java/selen/util/BrowserContent.java
+++ selen-lib/src/test/java/selen/util/BrowserContent.java
@@ -9,10 +9,31 @@ import org.openqa.selenium.WebElement;
 @RequiredArgsConstructor
 public class BrowserContent {
     private final WebDriver driver;
+    private final String JS_TOOLS =
+            "const $$ = (css, parent = document) => Array.from(parent.querySelectorAll(css))\n" +
+            "const $ =  (css, parent = document) => parent.querySelector(css)";
 
-    public void setBody(String html) {
+    public BrowserContent setBody(String html) {
         WebElement bodyEl = driver.findElement(By.cssSelector("html>body"));
         ((JavascriptExecutor) driver).executeScript("arguments[0].innerHTML = arguments[1]", bodyEl,
                 html);
+        return this;
+    }
+
+    public BrowserContent addJsTools() {
+        setScriptTag(JS_TOOLS);
+
+        return this;
+    }
+
+    public BrowserContent setScriptTag(String scriptContent) {
+        String escapedScriptContent = scriptContent.replaceAll("`", "\\`");
+        final String ADD_SCRIPT =
+                "const s = document.createElement('script')\n" +
+                "s.innerHTML = `" + escapedScriptContent + "`;\n" +
+                " document.querySelector('head').append(s);";
+
+        ((JavascriptExecutor) driver).executeScript(ADD_SCRIPT);
+        return this;
     }
 }
