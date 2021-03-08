a3cb205a45486c7ecb636ac3af18e6bc94b88dd8
Piotr Kluz <piox89@gmail.com>
2021-03-08 18:10:33 +0100

    Use tmp directory to store driver connection file.

--- selen-lib/src/main/java/selen/driver/ConnectionStore.java
+++ selen-lib/src/main/java/selen/driver/ConnectionStore.java
@@ -15,16 +15,14 @@ import java.nio.file.Path;
  * Store driver connection info in file
  */
 public class ConnectionStore {
-    private final Path path = Path.of(System.getProperty("user.home") + File.separator + "selen_browser_connection.txt");
+    private final Path path = Path.of(System.getProperty("java.io.tmpdir") + File.separator + "selen_browser_connection.txt");
 
     public String getConnectionString() throws IOException {
         return Files.readAllLines(path).get(0);
     }
 
     public void storeConnectionString(RemoteWebDriver driver) throws IOException {
-        String connectionString = getConnectionString(driver);
-        String FILE_INFO = "\r\nFile is safe to delete.";
-        Files.writeString(path, connectionString + FILE_INFO);
+        Files.writeString(path, getConnectionString(driver) + "\r\nFile is safe to delete.");
     }
 
     @NotNull
